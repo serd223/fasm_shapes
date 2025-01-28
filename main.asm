8d6eed93674a67fd3ba424209a90b61b1703615b
Serdar Çoruhlu <serdar.coruhlu@hotmail.com>
2025-01-28 14:31:20 +0300

    change draw_line to use the integer incremental error method

--- main.asm
+++ main.asm
@@ -20,6 +20,7 @@ mov dl, 1
 call write_string
 
 start:
+
         inc BYTE [draw_color]
         mov ax, 50
         mov bx, 30
@@ -48,7 +49,6 @@ start:
         mov dx, 100
         call draw_line
 
-
 jmp start
 
 ;; Input:
@@ -61,63 +61,106 @@ jmp start
 draw_line:
         ;; https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
 
-        ;; make sure x1 > x0
-        cmp ax, cx
-        jge .x_if_over
+        ;; make sure y1 > y0
+        cmp bx, dx
+        jge .y_if_over
         xchg ax, cx
         xchg bx, dx
-        .x_if_over:
+        .y_if_over:
 
-        sub bx, dx            ;; bx => dy = y1 - y0
-        add bx, bx            ;; bx => 2dy = dy + dy
-        mov si, bx            ;; si => 2dy
-        sub ax, cx            ;; ax => dx = x1 - x0
-        mov bx, ax            ;; bx => dx
-        mov di, cx            ;; di => x0
-        mov cl, 1             ;; if dy >= 0 { sign = 1 }
-        cmp si, 0             ;; 
-        jge .over             ;;
-        neg si                ;;
-        mov cl, -1            ;; else if dy < 0 { si = -si; sign = -1 }
-        .over:                ;;
+        ;; ah => sign_x 
+        ;; al => color 
+        ;; bx => error 
+        ;; cl => -d_y
+        ;; ch => abs(y1 - y0) (counter) 
+        ;; dx => d_x 
+        ;; si => abs(x1 - x0) (counter) 
+        ;; di => pointer 
 
-        mov ax, dx            ;; ax => y0
-        mov dx, display_width ;; dx => display_width
-        mul dx                ;; ax => y0 * display_width; dx => 0 (most of the time)
-        add di, ax            ;; di => y0 * display_width + x0
-        mov ah, cl            ;; ah => sign
-        mov cx, bx            ;; cx => dx
-        add cx, cx            ;; cx => 2dx
-        sub cx, si            ;; cx => 2dx - abs(2dy)
-        mov dx, si            ;; dx => abs(2dy)
-        sub dx, bx            ;; dx => abs(2dy) - dx
-
-        mov al, [draw_color]
+        mov di, cx              ;; di => x0
+        sub ax, cx              ;; ax => x1 - x0
+        mov si, ax              ;; si => x1 - x0
+        mov ax, dx              ;; ax => y0
+        mov cx, ax              ;; cx => y0
+        mov dx, display_width   ;; dx => display_width
+        mul dx                  ;; ax => y0 * display_width; dx => 0 (most of the time)
+        add di, ax              ;; di => y0 * display_width + x0
+        mov ah, 1               ;; ah => sign_y
+        cmp si, 0               ;; if x1 < x0 { sign_x = -1 } else { sign_x = 1 }
+        jge .sign_y_over
+        neg si
+        mov ah, -1
+        .sign_y_over:           ;; si => abs(x1 - x0)
+        mov dx, si              ;; dx => d_x = abs(x1 - x0)
+        sub bx, cx              ;; bx => abs(y1 - y0) (always positive)
+        mov ch, bl              ;; ch => abs(y1 - y0) (since the resolution is low, the y pos should fit in 8 bits)
+        mov cl, bl              ;; cl => -d_y = abs(y1 - y0)
+                                ;; bx => -dy
+        neg bx                  ;; bx => dy
+        add bx, dx              ;; bx => error = d_x + d_y
+        mov al, [draw_color]    ;; al => color
         .loop:
-                mov [es:di], al
-
-                cmp dx, 0
-                jle .neg
+                mov [es:di], al ;; plot(x0, y0)
+                push di         ;; [pixel]
+                mov di, bx
+                add bx, bx
+                push cx         ;; [dy|dyi][pixel]
+                xor ch, ch
+                neg cx
+                cmp bx, cx
+                pop cx          ;; [pixel]
+                jge .e2_dy_ge
+                jmp .e2_dy_else
+                .e2_dy_ge:
+                        cmp si, 0
+                        jle .over
+                        push cx         ;; [dyi|dy][pixel]
+                        xor ch, ch
+                        sub di, cx      ;; error -= -dy := error += dy
+                        pop cx          ;; [pixel]
+                        mov bx, di
+                        pop di          ;; []
                         cmp ah, 0
-                        jl .flag_neg
-                        jmp .flag_pos
-                        .flag_neg:
-                                sub di, display_width ;; go up
-                                jmp .flag_over
-                        .flag_pos:
-                                add di, display_width ;; go down
-                        .flag_over:
-                        sub dx, cx
-                        jmp .over_if
-                .neg:
-                        add dx, si
-                .over_if:
+                        jge .sign_x_pos
+                        jmp .sign_x_neg
+                        .sign_x_pos:
+                                inc di
+                                dec si
+                                jmp .sign_x_over
+                        .sign_x_neg:
+                                dec di
+                                dec si
+                        .sign_x_over:
+                        jmp .e2_dy_over
+                .e2_dy_else:
+                        mov bx, di
+                        pop di          ;; []
+                .e2_dy_over:
+                push di         ;; [pixel]
+                mov di, bx
+                add bx, bx
+                cmp bx, dx
+                jle .e2_dx_le
+                jmp .e2_dx_else
+                .e2_dx_le:
+                        cmp ch, 0
+                        jle .over
 
-                inc di ;; go right
-                dec bx
-                cmp bx, 0
-                jg .loop
+                        add di, dx
+                        mov bx, di
+                        pop di
+                        add di, display_width
+                        dec ch
 
+                        jmp .e2_dx_over
+                .e2_dx_else:
+                        mov bx, di
+                        pop di
+                .e2_dx_over:
+
+                jmp .loop
+        .over:
+        pop di
         ret
 
 
